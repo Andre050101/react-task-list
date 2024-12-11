92d4ddc03a55aceada77364f76981af7524d8fa4
Andre050101 <agiancristiano90@gmail.com>
2024-12-11 16:51:25 +0100

    design added in App.css

--- src/components/TaskListComponent.jsx
+++ src/components/TaskListComponent.jsx
@@ -7,25 +7,26 @@ const TaskListComponent = () => {
 
     const backlogOrProgressList = backlogOrProgress.map(task => (
         <li key={task.id}>
-            <strong>{task.title}</strong> - Priority: {task.priority}, Estimated Time: {task.estimatedTime} mins
+            <strong>{task.title}</strong> <span className="flag">{task.state}</span> <br></br> Priority: {task.priority} <br></br> Estimated Time: {task.estimatedTime}
         </li>
     ));
     const completedList = completed.map(task => (
         <li key={task.id}>
-            <strong>{task.title}</strong> - Priority: {task.priority}, Estimated Time: {task.estimatedTime} mins
+            <strong>{task.title}</strong> <span className="flag">{task.state}</span> <br></br> Priority: {task.priority}  <br></br> Estimated Time: {task.estimatedTime}
         </li>
     ));
 
     return (
         <div>
             <h1>Task Manager</h1>
-            <div>
+            <div className="tasks">
                 <h2>Current Tasks ({backlogOrProgress.length})</h2>
                 <ul>
                     {backlogOrProgressList}
                 </ul>
             </div>
-            <div>
+            <hr></hr>
+            <div className="tasks">
                 <h2>Completed Tasks ({completed.length})</h2>
                 <ul>
                     {completedList}
